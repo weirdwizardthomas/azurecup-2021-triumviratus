278ff59e2f41ae0bd696cb0977034f7c104fd7c6
weirdwizardtom <tuomas.koristka@gmail.com>
2021-05-31 19:26:12 +0200

    Added a cors treatment to the evaluate method

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,28 +1,41 @@
-from flask import Flask, request
+import flask
 
-import file_handler
 from classifier import Classifier
 from config import config
 from image import load_image
+import file_handler
+import json
 
-app = Flask(__name__)
+ACCESS_CONTROL_ALLOW_ORIGIN = '*'
+CONTROL_REQUEST_HEADERS = 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, ' \
+                          'Access-Control-Request-Method, Access-Control-Request-Headers '
+
+app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = config.classification.upload_folder
+app.config['CORS_HEADERS'] = 'Content-type'
+classifier = Classifier.get_instance()
 
 
 @app.route('/evaluate', methods=['GET', 'POST'])
 def evaluate():
+    request = flask.request
+
     if request.method == 'POST':
-        filename = file_handler.read(request.files['file'])
+        filename = file_handler.read(request.files['File'])
         image = load_image(filename)
         file_handler.remove(filename)
         predictions = {key: str(value) for key, value in classifier.predict(image).items()}
 
-        return {
+        response = flask.Response(json.dumps({
             'name': filename,
             'predictions': predictions
-        }
+        }))
+
+        response.headers['Access-Control-Allow-Origin'] = ACCESS_CONTROL_ALLOW_ORIGIN
+        response.headers['Access-Control-Allow-Headers'] = CONTROL_REQUEST_HEADERS
+
+        return response
 
 
 if __name__ == '__main__':
-    classifier = Classifier.get_instance()
-    app.run(host=config.server.host, port=config.server.port, debug=config.server.debug)
+    app.run(host='localhost', port=3001, debug=True)
--- backend/src/app_testovaci.py
+++ backend/src/app_testovaci.py
@@ -1,32 +1,25 @@
 from flask import Flask, request
-
-#from classifier import Classifier
+from flask_cors import CORS, cross_origin
 from config import config
-from image import load_image
-import file_handler
-import json
 
 app = Flask(__name__, template_folder='template')
-app.config['UPLOAD_FOLDER'] = config.classification.upload_folder
 
-#classifier = Classifier.get_instance()
+app.config['UPLOAD_FOLDER'] = config.classification.upload_folder
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+CORS(app, resources={r'/evaluate': {'origins': 'http://localhost:port'}})
 
 
 @app.route('/evaluate', methods=['GET', 'POST'])
+@cross_origin(origin='localhost',headers=['Content-Type', 'Authorization'])
 def evaluate():
     if request.method == 'POST':
-        
         print((request.form['File']))
         # print(request.data)
-        #filename = file_handler.read(request.form['File'])
-        #image = load_image(filename)
-        #file_handler.remove(filename)
- #       predictions = {key: str(value) for key, value in classifier.predict(image).items()}
-        #print(filename)
-        filename = 'asd'
+        filename = 'empty string'
         return {
             'name': filename,
-            'predictions': {}#predictions
+            'predictions': {}
         }
 
 
